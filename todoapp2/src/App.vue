0be5bc86860f33ca2d3e0cc153aba4ff74be907c
Lizosip <liza20070104@gmail.com>
2025-03-16 16:33:37 +0300

    first commit

---
+++ todoapp2/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class = "app_container">
+    <router-view />
+    <Navbar />
+  </div>
+</template>
+
+<script>
+import Navbar from '.components/Navbar.vue'
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  }
+
+}
+</script>
+
+<style scoped>
+.app-container {
+  display: flex;
+  flex-direction: column;
+  height: 98vh;
+}
+</style>
---
+++ todoapp2/src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+ <nav class="navbar">
+    <router-link to="/" class="nav-link">Задачи</router-link>
+    <router-link to="/profile" class="nav-link">Профиль</router-link>
+ </nav>
+</template>
+
+
+<script>
+export default {
+    name: "Navbar"
+}
+</script>
+
+<style scoped>
+.navbar {
+    display: flex;
+    justify-content: space-around;
+    aligh-atems: center;
+    height: 60px;
+
+.nav-link{
+    text-decoration: none;
+    color: #333;
+    font-size: 16px;
+    font-weight: 500;
+}
+}
+</style>
